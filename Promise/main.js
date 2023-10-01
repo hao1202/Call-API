efe065bd49889c9617618b2134a75ad7abb2d2cf
Tran Van Hao <tranvanhao2122003@gmail.com>
2023-10-01 22:34:54 +0700

    initialize promise

---
+++ Promise/main.js
@@ -0,0 +1,11 @@
+// Khởi tạo promise bằng class có sẵn Promise
+const promise = new Promise(
+    /*
+        chuyền vào executor nhận vào 2 tham số 
+        - resolve : là hàm sẽ được gọi khi promise hoàn thành
+        - reject : là hàm được gọi khi lỗi xảy ra
+    */
+    (resolve, reject) => {
+
+    }
+);
