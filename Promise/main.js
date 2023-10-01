456b76a461e770c24ade0b5fc61b62a38736232d
Tran Van Hao <tranvanhao2122003@gmail.com>
2023-10-02 01:17:15 +0700

    callback và callback hell

--- Promise/main.js
+++ Promise/main.js
@@ -21,6 +21,41 @@
     - Promise là đối tượng JS để thực hiện bất đồng bộ
 
 */
+/*
+    callback là một cách hiệu quả khi muốn trì hoãn một function 
+    nào đó và chờ cho đến khi một function khác thực thi và trả về kết quả
+*/
+
+// callAPI
+function httpGetAsync(theUrl, callback) {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = () => {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            callback(xmlHttp);
+        }
+    };
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+}
+httpGetAsync('https://picsum.photos/200/300', (data) => {
+    console.log(data);
+});
+/*
+    Nhưng dùng callback lại xảy ra một trường hơp đó là callback hell
+    => Tạo ra những đoạn code nồng vào nhau rất khó để bảo trì sau này
+    Để xử lí vấn đề này người ta dùng promise
+*/
+httpGetAsync('https://picsum.photos/200/300', (data) => {
+    document.getElementById('img_1').setAttribute('src', data.responseURL);
+    httpGetAsync('https://picsum.photos/200/300', (data) => {
+        document.getElementById('img_2').setAttribute('src', data.responseURL);
+        httpGetAsync('https://picsum.photos/200/300', (data) => {
+            document.getElementById('img_3').setAttribute('src', data.responseURL);
+        });
+    });
+})
+
+
 
 
 // Khởi tạo promise bằng class có sẵn Promise
